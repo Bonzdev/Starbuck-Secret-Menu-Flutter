2f510ec1257af74b944a92a0410a723ec91b70d7
arsibona <arsipbona@gmail.com>
2022-04-27 18:00:11 +0700

    Top Home

--- lib/views/components/top_home.dart
+++ lib/views/components/top_home.dart
@@ -1,59 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 class TopHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Container(margin:EdgeInsets.only(left: 8, right: 8),padding: EdgeInsets.only(top: 50,bottom: 20),child: Row(
       mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
-        Expanded(
-          flex: 2,
-          child: Container(
-            child: FadeInImage.assetNetwork(
-              alignment: Alignment.topCenter,
-              placeholder: "https://picsum.photos/100",
-              image: "https://picsum.photos/100",
-              fit: BoxFit.none,
-            ),
-          ),
-        ),
         Expanded(
           flex: 4,
           child: Container(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Container(child: Text("Grade:")),
-                    Container(child: Text("Sashimi")),
-                  ],
-                ),
+                Container(child: Text("Good Morning",style: GoogleFonts.openSans(
+                  textStyle: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),)),
                 SizedBox(height: 5.0),
 
-                Row(
-                  children: <Widget>[
-                    Container(child: Text("Spec:")),
-                    Container(child: Text("Skinless Boneless, Full Loins")),
-                  ],
-                ),
-                SizedBox(height: 5.0),
-
-                Row(
-                  children: <Widget>[
-                    Container(child: Text("Size:")),
-                    Container(child: Text("2-4 kG")),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Spacer(flex: 4,)
-                  ],
-                )
+                Container(child: Text("A good day starts with a good coffee.How do you want to start your day?",style: GoogleFonts.openSans(
+                  textStyle: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.normal,
+                  ),
+                ),)),
               ],
             ),
           ),
-        )
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: Image.network('https://res.cloudinary.com/bonzdev/image/upload/v1651057114/eastwood-starbucks_fyamqz.png', fit: BoxFit.cover, width: 50.0)
+          ),
+        ),
       ],
-    );
+
+    ),);
   }
 }
